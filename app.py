5849005f8e11f655eff6a3c1175f864a45dc1214
Pablo Miro <45178178+Paramiru@users.noreply.github.com>
2021-02-06 21:39:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv();
 
 def create_app():
     app = Flask(__name__)
-    # client = MongoClient(os.environ.get("MONGODB_URI"))
-    client = MongoClient("mongodb+srv://example:example@microblog.2joow.mongodb.net/test")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
@@ -24,7 +26,7 @@ def create_app():
             )
             for entry in app.db.entries.find({})
         ]
-        # entries_with_date_reversed = reversed(entries_with_date)
-        return render_template("home.html", entries=entries_with_date)
+        entries_with_date_reversed = reversed(entries_with_date)
+        return render_template("home.html", entries=entries_with_date_reversed)
     
     return app
